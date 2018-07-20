5a15ea3f0ec7a6bd10207dcae7d38c2df0eb525c
shlomograch <shlomegrach@gmail.com>
2018-07-20 10:46:42 -0600

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace APILoadTest
+{
+    class Class1
+    {
+        public async void MainMethod()
+        {
+            counter = 0;
+            var sw = Stopwatch.StartNew();
+
+            //Create Actions
+            var actions = Enumerable.Range(0, 10000)
+                .Select(i => ((Action)(() => DoSomething(i))));
+
+            //Run all parallel with 25 Tasks-in-parallel
+            await DoAll(actions, 1000);
+
+            Console.WriteLine("Total Time: " + sw.ElapsedMilliseconds);
+        }
+
+        public static int counter { get; set; }
+
+
+        public void DoSomething(int i)
+        {
+            var result = ApiServices.ApiGet("http://dev.intsvc.nelnet.net:4106/api/v1/states/validate/", "USA/CO");
+            Console.WriteLine("Result: " + result.StatusCode + " count - " + counter);
+            counter++;
+        }
+
+        public async Task DoAll(IEnumerable<Action> actions, int maxTasks)
+        {
+            SemaphoreSlim semaphore = new SemaphoreSlim(maxTasks);
+
+            foreach (var action in actions)
+            {
+                semaphore.WaitAsync().ConfigureAwait(false);
+                Task.Factory.StartNew(() => action(), TaskCreationOptions.LongRunning)
+                    .ContinueWith((task) => semaphore.Release());
+            }
+
+            for (int i = 0; i < maxTasks; i++)
+                semaphore.WaitAsync().ConfigureAwait(false);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using RestSharp;
+
+namespace APILoadTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Debug.WriteLine("Starting test...");
+
+
+            Class1 t = new Class1();
+            t.MainMethod();
+
+            //int CurCnt = 1;
+            //int MaxCnt = 5;
+
+
+            //var taskTimer = Task.Factory.StartNew(async () =>
+            //    {
+            //        var result = ApiServices.ApiGet("http://dev.intsvc.nelnet.net:4106/api/v1/states/validate/", "USA/CO");
+            //        Debug.WriteLine("Result: " + result.StatusCode + "   Count = " + CurCnt);
+            //        CurCnt++;
+
+            //    },
+            //    TaskCreationOptions.LongRunning);
+
+
+            Debug.WriteLine("Ending test...");
+            Console.ReadLine();
+        }
+    }
+
+
+    public static class ApiServices
+    {
+        internal static string UriBase { get; set; }
+        internal static string UriMethodPath { get; set; }
+        private static RestClient Client { get; set; }
+
+        public static IRestResponse ApiGet(string baseUri, string uriMethodPath)
+        {
+            UriBase = baseUri;
+            UriMethodPath = uriMethodPath;
+
+            try
+            {
+                Client = new RestClient(UriBase);
+                var restRequest = new RestRequest(UriMethodPath, Method.GET);
+                restRequest.AddHeader("Accept", "application/json");
+                restRequest.AddHeader("UserName", "");
+                restRequest.AddHeader("StarKey", "");
+                return Client.Execute(restRequest);
+            }
+
+            catch (Exception exception)
+            {
+                Debug.WriteLine(exception.Message);
+            }
+
+            return null;
+        }
+    }
+}
